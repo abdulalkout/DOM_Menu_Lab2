9e1af24185de203b0ce642c24745f87f08d9b47d
abdulalkout <abdul.alkout@gmail.com>
2023-10-31 20:04:36 -0500

    All tasks untill 5.6 were done

---
+++ script.js
@@ -0,0 +1,95 @@
+// Data Structure
+var menuLinks = [
+  { text: "about", href: "/about" },
+  {
+    text: "catalog",
+    href: "#",
+    subLinks: [
+      { text: "all", href: "/catalog/all" },
+      { text: "top selling", href: "/catalog/top" },
+      { text: "search", href: "/catalog/search" },
+    ],
+  },
+  {
+    text: "orders",
+    href: "#",
+    subLinks: [
+      { text: "new", href: "/orders/new" },
+      { text: "pending", href: "/orders/pending" },
+      { text: "history", href: "/orders/history" },
+    ],
+  },
+  {
+    text: "account",
+    href: "#",
+    subLinks: [
+      { text: "profile", href: "/account/profile" },
+      { text: "sign out", href: "/account/signout" },
+    ],
+  },
+];
+
+// Queries
+const mainEl = document.querySelector("main");
+const header = document.querySelector("header");
+const topMenuEl = document.querySelector("#top-menu");
+const subMenuEl = document.querySelector("#sub-menu");
+let showingSubMenu = false;
+
+// Functions
+mainEl.style.backgroundColor = "var(--main-bg)";
+mainEl.innerHTML = "<h1>SEI Rocks!</h1>";
+mainEl.className = "flex-ctr";
+
+// ------------------------------------TopElement-------------------
+topMenuEl.className = "flex-around";
+topMenuEl.style.height = "100%";
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+
+//-----------------------------------subMenuEl----------------------
+subMenuEl.style.height = "100%";
+subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
+subMenuEl.className = "flex-around";
+subMenuEl.style.position = "absolute";
+subMenuEl.style.top = "0";
+
+menuLinks.forEach((element) => {
+  let newAnc = document.createElement("a");
+  newAnc.href = element.href;
+  newAnc.textContent = element.text;
+  topMenuEl.appendChild(newAnc);
+});
+let topMenuLinks = document.querySelectorAll("a");
+// console.log(topMenuLinks);
+
+// Event Listners
+topMenuEl.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.tagName !== "A") {
+    return;
+  }
+  console.log(e.target);
+  // console.log(e.target.tagName);
+  if (e.target.className === "active") {
+    e.target.classList.remove("active");
+    showingSubMenu = false;
+    subMenuEl.style.top = "0";
+    return;
+  }
+  console.log(topMenuLinks[1]);
+  console.log(e.target.textContent);
+
+  topMenuLinks.forEach((topMenuATag) => {
+    topMenuATag.classList.remove("active");
+  });
+
+  e.target.className = "active";
+
+  menuLinks.forEach((data) => {
+    if (e.target.textContent === data.text) {
+      if (data.subLinks) {
+        showingSubMenu = true;
+      }
+    }
+  });
+});
